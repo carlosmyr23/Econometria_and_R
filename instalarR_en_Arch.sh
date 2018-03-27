9449ed5df7a8e53813a68b88eeef4eebb9aa1345
carlosmyr23 <carlosmyr23@gmail.com>
2018-03-26 20:17:00 -0600

    Update and rename instalarR.sh to instalarR_en_Arch.sh

--- instalarR_en_Arch.sh
+++ instalarR_en_Arch.sh
@@ -1,4 +1,5 @@
 
 echo "Este script invoca a R e instala los paquetes necesarios para analizar datasets económicos para Arch Linux"
 sudo Pacman -S R 
+echo "Funciona mejor para mi el binario"
 yaourt -S rstudio-desktop-bin
